8314442dd34fe9f18b0616d74c2f044a60172d43
LitanMolla <litanmolla9@gmail.com>
2026-07-31 22:18:01 +0600

    updated: added trycatch in logoutController

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -73,15 +73,19 @@ const loginController = async (req, res) => {
 }
 
 const logoutController = async (req, res) => {
-    const { email } = req.params
-    if (!email) {
-        return res.send('email required')
+    try {
+        const { email } = req.params
+        if (!email) {
+            return res.status(400).json({ success: true, message: 'email required' })
+        }
+        const user = await User.findOneAndUpdate({ email }, { isLogin: false })
+        if (!user) {
+            return res.status(400).json({ success: false, message: 'user not found' })
+        }
+        return res.send({success:true,message: 'Logout success'})
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message })
     }
-    const user = await User.findOneAndUpdate({ email }, { isLogin: false })
-    if (!user) {
-        return res.send('user not found')
-    }
-    return res.send('Logout success')
 }
 
 module.exports = { sentOTPController, loginController, logoutController }
